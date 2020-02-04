f6b428c0f3e354458ce4622ec776e76c18e03426
Patrice <patweb44@gmail.com>
2020-02-04 18:00:02 +0100

    Setting up project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpModule } from "@angular/http";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { CubeComponent } from "./cube/cube.component";
+import { PanoramaEquirectangularComponent } from "./panorama-equirectangular/panorama-equirectangular.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CubeComponent, PanoramaEquirectangularComponent],
+  imports: [BrowserModule, HttpModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
